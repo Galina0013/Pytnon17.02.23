8e869f27975cc729ad63cd814c905e50d6f36375
Galina0013 <113652966+Galina0013@users.noreply.github.com>
2023-02-22 10:09:13 +0300

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,4 @@
+time = int(input("Ведите время в секундах: "))
+h = round(time/3600, 1)
+m = round(time/60, 1)
+print(h, ": ", m, ": ", time)
---
+++ Ex3.py
@@ -0,0 +1,6 @@
+n = input("введите целое положительное число: ")
+nn = int(n + n)
+nnn = int(n + n + n)
+print(int(n) + nn + nnn)
+
+
